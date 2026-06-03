a2cb224521a80484884ece4a48a074d5bf5cc26b
Ceeli06 <cherylli621@gmail.com>
2026-06-03 17:15:40 +0200

    Wrote text files for the 3 layouts (large/medium/small)

--- helper.py
+++ helper.py
@@ -1,2 +1,34 @@
 # Will include functions for setup of large, medium, and small store layouts
 
+#large function
+# what we want to do:
+
+
+#params:
+#numpeople, numrobots, sizeofstore, sizeoforders, numtotalorders, custumor disruption percentage
+# constants: freezer perishable time (30m), order due time (4hr), walking speed (1m/s), 
+# break time (30 min break in mid of shift), shift schedule (6-2pm, 2-10pm), charging/downtime for AMR, AMR speed 
+
+
+
+categoryMapping = {
+    1: "Grocery",
+    2: "Perishable Grocery",
+    3: "Health & Beauty",
+    4: "Misc.",
+    5: "Fashion",
+    6: "Home",
+    7: "Cleaning",
+    8: "Toys",
+    9: "Pets",
+    10: "Electronics",
+    11: "Garden",
+    12: "Home Improvement",
+    13: "Auto",
+    14: "Sports & Outdoors",
+    15: "Arts & Crafts",
+    "N": "Empty",
+    "S": "Staging"
+}
+
+
