6c76aeedbdd1076b020e33b944716b261d50f83c
Ike S. <stuhr037@umn.edu>
2026-06-04 14:33:14 -0500

    changed comments for clarity

--- DatasetAnalysis/getOrderSizeFreq.py
+++ DatasetAnalysis/getOrderSizeFreq.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-# Load the data one last time
+# Load dataset
 df = pd.read_csv("OrderDataset(Mapped).csv")
 size_counts = df.groupby("VisitNumber").size().value_counts().sort_index()
 
-# Generate copy-pasteable Python code
+# Output dictionary of orderSize : frequencies
 print("order_distribution = {")
 for size, count in size_counts.items():
     print(f"    {size}: {count},")
