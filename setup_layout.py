b7c02ef5c4dc81885c552fadbc762acae51855d1
Ceeli06 <cherylli621@gmail.com>
2026-06-10 20:29:00 +0200

    Set up routing (distance map of all walkable coordinates to each other based on BFS algorithm)

--- setup_layout.py
+++ setup_layout.py
@@ -2,9 +2,11 @@
 # Includes mapping function to create dictionary of aisle -> coordinates, which will be used for order generation
 
 import numpy as np
-from collections import defaultdict
+from collections import defaultdict, deque
 
-categoryMapping = {
+# Key for understanding .txt file to layout conversion
+# . is walking space
+CATEGORYMAPPING = {
     1: "Grocery",
     2: "Perishable Grocery",
     3: "Health & Beauty",
@@ -57,4 +59,47 @@ def setup_small():
         return layout_to_array(small_layout)
     
 
+#Breadth-first search to find shortest distance between the given point and every other point in coordinate
+def distance_map(grid, start):
+    rows, cols = grid.shape
 
+    dist = np.full((rows, cols), -1, dtype=int)
+
+    q = deque([start])
+    dist[start] = 0
+
+    directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+    while q:
+        r, c = q.popleft()
+
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+
+            if (
+                0 <= nr < rows and
+                0 <= nc < cols and
+                grid[nr, nc] == "." and
+                dist[nr, nc] == -1
+            ):
+                dist[nr, nc] = dist[r, c] + 1
+                q.append((nr, nc))
+    return dist
+
+
+# Sets up a dictionary of coordinates (r, c) to an array of distances to other coordinates
+def all_distance_maps(grid):
+    rows, cols = grid.shape
+    result = {}
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r, c] == ".":
+                result[(r, c)] = distance_map(grid, (r, c))
+
+    return result
+
+dist_map = all_distance_maps(setup_small())
+
+print(dist_map[(2, 2)][2,0])
+print(dist_map[(2,0)][4,17])
