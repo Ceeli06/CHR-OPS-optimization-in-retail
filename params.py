474a6ec91cb5e7bafef8bda34fa5188d0365db9b
Ieidtogi <stuhr037@umn.edu>
2026-06-11 09:24:55 -0500

    Added params.py and orders.json+builder

---
+++ params.py
@@ -0,0 +1,15 @@
+#simulation parameters
+num_pickers = 1
+num_robots = 1
+num_customers = 1 #representative of overall store congestion
+
+#simulation constants
+FREEZER_PERISHABLE_TIME = 30 * 60 #30 minute time limit from pick to staging for perishable grocery items before they are considered spoiled
+ORDER_DUE_TIME = 4 * 60 * 60 # 4 hour time limit from order generation to completion before it is considered late
+STAGING_TIME = 2 * 60 * 60 # Simulation assumes staging is a flat 2 hour process
+WALKING_SPEED = 1.0 # human walking speed = 1 meter per second
+HUMAN_PICK_TIME = 20 # seconds to pick an item by a human = 20 seconds
+AMR_LOAD_TIME = 10 # seconds to load an item onto the AMR = 10 seconds
+AMR_UNLOAD_TIME = 10 # seconds to unload an item from the AMR = 10 seconds
+AMR_CAPACITY = 10 # maximum number of items an AMR can carry = 10 items
+AMR_SPEED = 1.5 # AMR movement speed = 1.5 meters per second
