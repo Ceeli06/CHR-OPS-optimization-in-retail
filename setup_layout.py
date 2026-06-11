d7e2361aeb82833ae1d584473297067778d1f1b6
Ceeli06 <cherylli621@gmail.com>
2026-06-11 15:44:49 +0200

    Set up nearest neighbor heuristic for routing

--- setup_layout.py
+++ setup_layout.py
@@ -43,7 +43,6 @@ def map_of_coords(layout):
 
     return dict(coord_map)
 
-
 def setup_large():
     with open("Layouts/large.txt", "r") as f:
         large_layout = f.read() 
@@ -79,7 +78,7 @@ def distance_map(grid, start):
             if (
                 0 <= nr < rows and
                 0 <= nc < cols and
-                grid[nr, nc] == "." and
+                (grid[nr, nc] == "." or grid[nr,nc] == "S") and
                 dist[nr, nc] == -1
             ):
                 dist[nr, nc] = dist[r, c] + 1
@@ -94,12 +93,49 @@ def all_distance_maps(grid):
 
     for r in range(rows):
         for c in range(cols):
-            if grid[r, c] == ".":
+            if grid[r, c] == "." or "S":
                 result[(r, c)] = distance_map(grid, (r, c))
 
     return result
 
-dist_map = all_distance_maps(setup_small())
 
-print(dist_map[(2, 2)][2,0])
-print(dist_map[(2,0)][4,17])
+# orders will be a list of coordinates 
+def nearest_neighbor(orders, dist_map, staging):
+
+    unvisited = set(orders)
+
+    current = staging
+
+    path = [current]
+
+    while unvisited:
+        best_node = None
+        best_dist = float("inf")
+
+        for node in unvisited:
+            d = dist_map[current][node[0], node[1]]
+
+            # safety check (in case unreachable)
+            if d == -1:
+                continue
+
+            if d < best_dist:
+                best_dist = d
+                best_node = node
+
+        if best_node is None:
+            break  # remaining nodes unreachable
+
+        path.append(best_node)
+        unvisited.remove(best_node)
+        current = best_node
+
+    return path
+
+small = setup_small()
+dist_map = all_distance_maps(small)
+coord_map = map_of_coords(small)
+staging = coord_map["S"][0]
+route = nearest_neighbor([(1,1), (2,2), (3,3), (2,30), (6,70), (2,4), (5,80), (6,2)], dist_map, staging)
+route.append(staging)
+print(route)
