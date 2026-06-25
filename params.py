f14db35d8d0b7a729e0db12d0ce817170623d2d1
Ieidtogi <stuhr037@umn.edu>
2026-06-25 04:47:07 -0500

    updated params for realism

--- params.py
+++ params.py
@@ -1,33 +1,36 @@
+import random
+
 """Parameters"""
 
-num_pickers = 1
-num_robots = 3
-num_customers = 30  # Representative of store congestion
+num_pickers = 4 #default: 4 pickers
+num_robots = 4 #default: 4 AMRs
+num_customers = 30  # Representative of store congestion (default: 30 customers)
 order_arrival_rate = (
-    1 / 300
-)  # Avg. orders per second for arrivalTimeGen's Poisson process (default:1/300sec{1/5min or 12/hr}))
+    1 / 450
+)  # Avg. orders per second for arrivalTimeGen's Poisson process (default:1/450sec{8 orders/hour}))
 
 """Constants"""
 # Perishable items must reach depot within this time or spoil
-FREEZER_PERISHABLE_TIME = 30 * 60  # 30 minutes
+FREEZER_PERISHABLE_TIME = 60 * 60  # 1 hour
 # Orders exceeding this time are marked late
-ORDER_DUE_TIME = 4 * 60 * 60  # 4 hours (14400 seconds)
+ORDER_DUE_TIME = random.uniform(2*60*60, 4*60*60)  # 2-4 hours depending on customer shipping option choice
 STAGING_TIME = 2 * 60 * 60  # 2 hours
-WALKING_SPEED = 1.0  # Walking speed in meters per second
-HUMAN_PICK_TIME = 10  # Time to manually pick one item
-CART_LOAD_TIME = 10  # Seconds to load one item onto the cart/AMR
-AMR_UNLOAD_TIME = 10  # Seconds to unload one item
+WALKING_SPEED = 1.3  # Walking speed in meters per second
+HUMAN_PICK_TIME = 20  # Time to find and manually pick one item at the pick location
+CART_LOAD_TIME = 5  # Seconds to load one item onto the cart/AMR
+# Simulated seconds to unload one item (Reflects reality: unloading totes at 6 items/1 tote/5 seconds)
+AMR_UNLOAD_TIME = 0.8  
 AMR_CAPACITY = 50  # Maximum AMR item capacity per trip
 AMR_SPEED = 1.5  # AMR speed in meters per second
-CUSTOMER_SPEED = 0.8  # customer speed in meters per second
-CUSTOMER_BROWSE_TIME = 15  # Time a customer spends at each item coord browsing
+CUSTOMER_SPEED = 1  # customer speed in meters per second
+CUSTOMER_BROWSE_TIME = 30  # Time a customer spends at each item coord browsing
 CUSTOMER_COLLISION_TIME = 10  # Time AMR pauses upon colliding with a customer
 CUSTOMER_COLLISION_BUFFER = 15  # Seconds of margin added to each side of the AMR/customer occupancy overlap check
-BATCH_SIZE_MIN = 6  # Minimum number of orders per batch (group) in batching policies
-BATCH_SIZE_MAX = 8  # Maximum number of order per batch (group) in batching policies
+BATCH_SIZE_MIN = 4  # Minimum number of orders per batch (group) in batching policies
+BATCH_SIZE_MAX = 6  # Maximum number of order per batch (group) in batching policies
 SIM_TIME = 8 * 60 * 60  # Simulates 8 hour workday
 # Orders waiting longer than this are forceed into the next batch
-SIMILARITY_BATCH_MAX_WAIT = 90 * 60  # 1.5 hours
+SIMILARITY_BATCH_MAX_WAIT = 30 * 60  # 30 minutes
 # If the pending queue has been non-empty this long without reaching BATCH_SIZE_MIN, force a dispatch
 BATCH_TIMEOUT = 5 * 60  # 5 minutes
 URGENCY_THRESHOLD = (
